d0ac3d0ffc8fe717dd9f9ee9cb36d9858993cdf7
Nguyễn Đình Tạo <nuthancooltaoqn@gmail.com>
2020-04-08 12:22:26 +0700

    21312

---
+++ covid-map/.cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"tracking-coronavirus-COVID-19","short_name":"tracking-coronavirus-COVID-19","start_url":"/","background_color":"#FFFFFF","theme_color":"#00AFAA","display":"minimal-ui","icon":"src/images/gatsby-icon.png"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
